f1c867db5da5c054259d24428f03e84abe107bd7
noisegrrrl <agathe.boutmy@clever-cloud.com>
2018-11-28 16:03:45 +0100

    add warp10 plugins

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ yarn install --pure-lockfile
 yarn dev
 
 sed /http_port/s/3000/8080/ -i conf/defaults.ini
+
+cd $GOPATH/src/github.com/grafana/grafana/data/plugins
+
+git clone https://github.com/ovh/ovh-warp10-datasource.git
