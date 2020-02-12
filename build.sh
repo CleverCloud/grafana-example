1d2b1c5c659f6ca0bba99fe93900929b626c6ce5
Alexandre Berthaud <alexandre@berthaud.me>
2020-02-12 11:18:40 +0100

    Update build

--- build.sh
+++ build.sh
@@ -2,13 +2,26 @@
 
 go get github.com/grafana/grafana
 cd $GOPATH/src/github.com/grafana/grafana
-go run build.go setup
+if [[ -z "${GRAFANA_VERSION}" ]]; then
+  echo "No GRAFANA_VERSION environment variable set"
+  exit 1
+fi
+
+git checkout "${GRAFANA_VERSION}"
+go get -v all
 go run build.go build
 
+mkdir -p $GOPATH/bin/
+
+for bin in grafana-cli grafana-server; do
+  cp $GOPATH/src/github.com/grafana/grafana/bin/linux-amd64/${bin} $GOPATH/bin/
+done
+
 yarn install --pure-lockfile
-yarn dev
+yarn build
 
 mkdir -p data/plugins
 cd data/plugins
 
 git clone https://github.com/ovh/ovh-warp10-datasource.git
+
